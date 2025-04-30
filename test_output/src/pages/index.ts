413c42e7486dde1ccb4ca0fb557b6fe2596ce1e3
flemmrs-tl <flemming.bengtsen@tradeledger.io>
2025-04-30 14:56:12 +0100

    Add pages index for easy imports

---
+++ test_output/src/pages/index.ts
@@ -0,0 +1,21 @@
+// Export all page objects for easy imports
+import BasePage from './BasePage';
+import LoginPage from './LoginPage';
+import DashboardPage from './DashboardPage';
+import NewTaskPage from './NewTaskPage';
+
+// Export individual page objects
+export {
+  BasePage,
+  LoginPage,
+  DashboardPage,
+  NewTaskPage
+};
+
+// Export default object with all pages for convenience
+export default {
+  BasePage,
+  LoginPage,
+  DashboardPage,
+  NewTaskPage
+};
